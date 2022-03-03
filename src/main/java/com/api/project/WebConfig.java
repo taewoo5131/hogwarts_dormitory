4e5d79133c8b98b71feca1c96026ac1cf3e7d064
leetaewoo <taewoo5131@gmail.com>
2022-03-03 12:57:07 +0900

    - 회원가입 도입 중 : 회원가입 컨트롤러 호출 전 인터셉터 경로 main application과 같은 경로로 수정

--- src/main/java/com/api/project/WebConfig.java
+++ src/main/java/com/api/project/WebConfig.java
@@ -1,15 +1,21 @@
-package com.api.project.interceptor;
+package com.api.project;
 
+import com.api.project.interceptor.DormitoryCheckInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class interceptorConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+
         registry.addInterceptor(new DormitoryCheckInterceptor())
-                .order(1)
+//                .order(1)
                 .addPathPatterns("/**");
 //                .excludePathPatterns("/", ".members/add", "/login", "/logout", "/css/**", "/*.ico", "/error");
     }
--- src/main/java/com/api/project/interceptor/DormitoryCheckInterceptor.java
+++ src/main/java/com/api/project/interceptor/DormitoryCheckInterceptor.java
@@ -1,6 +1,7 @@
 package com.api.project.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,17 +12,19 @@ import javax.servlet.http.HttpServletResponse;
 public class DormitoryCheckInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        log.info("interceptor >> {}", request);
-        return false;
+        log.error("interceptor preHandle >> {}", request);
+        return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.error("interceptor postHandle >> {}", request);
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.error("interceptor afterCompletion >> {}", request);
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
 }
