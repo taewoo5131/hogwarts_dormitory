e3e856f1babaad7ba80afea99f85677bd235db3e
leetaewoo <taewoo5131@gmail.com>
2022-03-04 12:58:36 +0900

    - 회원가입 도입 중 : 인터셉터에서 dormitory 체크 후 없으면 "/dormitory" 로 리다이렉트

--- src/main/java/com/api/project/interceptor/DormitoryCheckinterceptorV2.java
+++ src/main/java/com/api/project/interceptor/DormitoryCheckinterceptorV2.java
@@ -11,16 +11,29 @@ import javax.servlet.http.HttpSession;
 @Slf4j
 @Component
 public class DormitoryCheckinterceptorV2 implements HandlerInterceptor {
+
+    /**
+     * desc session에 dormitoryName이 없으면 기숙사 배정을 받지 않았으므로 login 및 회원가입 등등 접근 불가
+     * @throws Exception
+     */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HttpSession session = request.getSession();
-        String dormitoryName = (String)session.getAttribute("dormitoryName");
+        String dormitoryName = (String) session.getAttribute("dormitoryName");
         log.info("인터셉터에서 session check => {} ", dormitoryName);
-        return false;
-//        if (dormitoryName.equals("") || dormitoryName.equals("0")) {
-//            return false;
-//        }else{
-//            return true;
-//        }
+        try {
+            if (dormitoryName.equals("") || dormitoryName.equals("0")) {
+                response.sendRedirect(request.getContextPath() + "/dormitory");
+                log.info("인터셉터에서 session check dormitoryName이 올바르지 않음. => {} ", dormitoryName);
+                return false;
+            } else {
+                return true;
+            }
+        } catch (NullPointerException e) {
+            response.sendRedirect(request.getContextPath() + "/dormitory");
+            log.error("인터셉터에서 dormitoryName Null");
+            return false;
+        }
+//        return true;
     }
 }
