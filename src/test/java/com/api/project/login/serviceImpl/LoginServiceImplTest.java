79894b94d5187338987247734164ae37a4230f86
leetaewoo <taewoo5131@gmail.com>
2022-03-30 23:25:22 +0900

    - ec2 build 위해 springBootTest 어노테이션 제거

--- src/test/java/com/api/project/login/serviceImpl/LoginServiceImplTest.java
+++ src/test/java/com/api/project/login/serviceImpl/LoginServiceImplTest.java
@@ -3,12 +3,11 @@ package com.api.project.login.serviceImpl;
 import com.api.project.user.serviceImpl.UserServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@SpringBootTest
+//@SpringBootTest
 class LoginServiceImplTest {
 
     @Autowired
